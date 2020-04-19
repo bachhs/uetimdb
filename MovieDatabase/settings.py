8e7e058cc63aff475ab15c66e9a29132903dfb88
bachhs <bachhs21@gmail.com>
2020-04-19 11:58:17 +0700

    change project name to "uetimdb"

--- MovieDatabase/settings.py
+++ MovieDatabase/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'MovieDatabase.urls'
+ROOT_URLCONF = 'uetimdb.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'MovieDatabase.wsgi.application'
+WSGI_APPLICATION = 'uetimdb.wsgi.application'
 
 
 # Database
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MovieDatabase.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uetimdb.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
