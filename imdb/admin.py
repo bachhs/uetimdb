feddae8a1513fa40b7cf5db57a9eabc4246897ae
bachhs <bachhs21@gmail.com>
2020-04-20 17:23:40 +0700

    update model

--- imdb/admin.py
+++ imdb/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Name, Genre
 
-admin.site.register(Movie)
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id_genre', 'nama_genre')
+    ordering = ('id_genre', 'nama_genre')
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id_movie', 'judul_movie',
+                    'tahun_movie', 'rating_movie')
+    list_filter = ('rating_movie',)
+    search_fields = ('judul_movie', 'id_movie')
+    ordering = ('judul_movie', 'rating_movie')
+
+
+@admin.register(Name)
+class NameAdmin(admin.ModelAdmin):
+    list_display = ('id_name', 'nama_name', 'dob_name')
+    ordering = ('nama_name', 'dob_name')
--- imdb/models.py
+++ imdb/models.py
@@ -20,3 +20,52 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.judul_movie
+
+
+class Genre(models.Model):
+    id_genre = models.IntegerField(primary_key=True)
+    nama_genre = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.nama_genre
+
+
+class Name(models.Model):
+    id_name = models.IntegerField(primary_key=True)
+    nama_name = models.CharField(max_length=100, null=True)
+    dob_name = models.DateField(null=True)
+    pob_name = models.CharField(max_length=500, null=True)
+    dod_name = models.DateField(null=True)
+    pod_name = models.CharField(max_length=500, null=True)
+    birth_name_name = models.CharField(max_length=200, null=True)
+    height_name = models.CharField(max_length=20, null=True)
+    bio_name = models.TextField(null=True)
+    poster_name = models.TextField(null=True)
+    link_name = models.TextField(null=True)
+
+    def __str__(self):
+        return self.nama_name
+
+
+class Movie_Cast(models.Model):
+    id_movie_cast = models.IntegerField(primary_key=True)
+    id_name = models.ForeignKey(
+        Name, on_delete=models.CASCADE, db_column='id_name')
+    id_movie = models.ForeignKey(
+        Movie, on_delete=models.CASCADE, db_column='id_movie')
+
+
+class Movie_Director(models.Model):
+    id_movie_director = models.IntegerField(primary_key=True)
+    id_name = models.ForeignKey(
+        Name, on_delete=models.CASCADE, db_column='id_name')
+    id_movie = models.ForeignKey(
+        Movie, on_delete=models.CASCADE, db_column='id_movie')
+
+
+class Movie_Genre(models.Model):
+    id_movie_genre = models.IntegerField(primary_key=True)
+    id_genre = models.ForeignKey(
+        Genre, on_delete=models.CASCADE, db_column='id_genre')
+    id_movie = models.ForeignKey(
+        Movie, on_delete=models.CASCADE, db_column='id_movie')
