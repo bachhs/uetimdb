d761d865597d4c800d8beeb89a4a87f33f4f3186
bachhs <bachhs21@gmail.com>
2020-04-26 16:57:24 +0700

    update detail view

--- imdb/views.py
+++ imdb/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Movie
+from .models import Movie, Movie_Cast, Movie_Director, Movie_Genre
 
 
 class MovieList(ListView):
@@ -10,4 +10,24 @@ class MovieList(ListView):
 
 class MovieDetail(DetailView):
     model = Movie
-    slug_field = 'id_movie'
+
+    def get_object(self):
+        object = super(MovieDetail, self).get_object()
+        return object
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cast_list'] = Movie_Cast.objects.filter(
+            id_movie=self.get_object().id_movie)
+        context['director_list'] = Movie_Director.objects.filter(
+            id_movie=self.get_object().id_movie)
+        context['genre_list'] = Movie_Genre.objects.filter(
+            id_movie=self.get_object().id_movie)
+        return context
+
+
+class MovieGenre(ListView):
+    model = Movie
+
+    def get_queryset(self):
+        return super().get_queryset()
